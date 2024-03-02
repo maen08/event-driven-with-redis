2fe01dc58d95ae00f94b38dbd7dba485fd8fbe02
maen08 <2001stany@gmail.com>
2024-03-02 16:10:30 +0300

    fix: publish user data to the redis stream

--- with-two-microservices/publisher-service.py
+++ with-two-microservices/publisher-service.py
@@ -1,9 +1,9 @@
-import requests
+
 from faker import Faker
 from flask import Flask
 import json
 import time
-from redis_om import get_redis_connection
+from redis_om import get_redis_connection, HashModel
 
 # Microservice A - service for creating users
 
@@ -21,11 +21,30 @@ class RedisNetwork:
 
     def create_stream_data(key:str, data_object:dict):
         redis_ = RedisNetwork.redis_connection()
-        stream_data = redis_.xadd(key, data_object, '*')
-        return stream_data
+        redis_.xadd(key, data_object, '*')
+        # return stream_data
 
 
-class Publish:
+class UserDataModel:
+    # save data in mysql db
+    db_connection = 'mysql_db'
+    pass
+
+
+class HashUserData(HashModel):
+    # redis does not accept raw json data or model - has to be hashed
+    name:str
+    phone:str
+    job:str
+    address:str
+
+    class Meta:
+        # this should be a real db eg. mysql
+        # database = UserDataModel.db_connection
+        database = RedisNetwork.redis_connection()
+
+
+class PublishUser:
     def create_user():
         fake = Faker()
         user = {
@@ -34,7 +53,7 @@ class Publish:
             'job': fake.job(),
             'address': fake.address()
         }
-        user = json.dumps(user)
+        # user = json.dumps(user) # convert dict to json data
         return user
     
 
@@ -43,14 +62,27 @@ class Publish:
         # Microservice A which creates users and save them in its own DB (assume mysql db and also it saves the event in redis).
         # Other microservices assume B which want to process user information as soon as user is created.
         # And thats where we use redis streaming between these two microservices.
-        # Why redis streaming ? - 
         
-        user_data = Publish.create_user()
+        user_data = PublishUser.create_user()
+        
+        # save data in the db eg. mysql
+        user_db = UserDataModel()
 
-        # publish user created event
-        print(user_data)
+        # then hash the data and publish the event
+        user = HashUserData(
+            name=user_data['name'],
+            phone=user_data['phone'],
+            job=user_data['job'],
+            address=user_data['address']
+        )
+        user.save()
+
+
         redis = RedisNetwork
-        redis.create_stream_data(key='user_created', data_object=user_data) 
+        redis.create_stream_data(
+            key='user_created',
+            data_object=user.__dict__ 
+        ) 
         return user_data
 
 
